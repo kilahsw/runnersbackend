d5ddb5b4363faaefb28fe8be0969e53b08369fa7
Kilah Willingham <kilahmay@kilahs-air.lan>
2020-10-20 18:00:41 -0700

    shoes controllers updated

--- controllers/shoes.js
+++ controllers/shoes.js
@@ -7,6 +7,20 @@ router.get("/", async (req, res) => {
     res.json(await Shoe.find({}));
 });
 
+//create route
+router.post("/", async (req, res) => {
+    res.json(await Shoe.create(req.body));
+});
+
+//update route
+router.put("/:id", async (req, res) => {
+    res.json(await Shoe.findByIdAndUpdate(req.params.id, req.body, { new: true }));
+});
+
+//delete route
+router.delete("/:id", async (req, res) => {
+    res.json(await Shoe.findByIdAndRemove(req.params.id));
+});
 
 
 
