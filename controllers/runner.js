b27cb4e4959340b8d9549ca341d5d40602115ebf
Kilah Willingham <kilahmay@kilahs-air.lan>
2020-10-20 17:25:23 -0700

    added mongouri

--- controllers/runner.js
+++ controllers/runner.js
@@ -7,6 +7,20 @@ router.get("/", async (req, res) => {
     res.json(await Runner.find({}));
 });
 
+//create route
+router.post("/", async (req, res) => {
+    res.json(await Runner.create(req.body));
+});
+
+//update route
+router.put("/:id", async (req, res) => {
+    res.json(await Runner.findByIdAndUpdate(req.params.id, req.body, { new: true }));
+});
+
+//delete route
+router.delete("/:id", async (req, res) => {
+    res.json(await Runner.findByIdAndRemove(req.params.id));
+});
 
 
 
